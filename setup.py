31572c3720a69dda5fa9ffaa4b797006760ea205
nirmalpatel12 <patelnirmal917>
2023-12-28 11:33:12 +0530

    Normalize line endings

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+setup(
+    name="Kidney Tumor Classifier",
+    version="0.0.1",
+    author="Patel Nirmalkumar Ketanbhai",
+    author_email="patelnirmal917@gmail.com",
+    
+    packages=find_packages()
+)
